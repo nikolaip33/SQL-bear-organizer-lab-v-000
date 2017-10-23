421f056f813dbf582baef37910ba17092b140bac
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-23 10:30:57 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender) values
+("Mr. Chocolate", 7, "M"),
+("Rowdy", 1, "M"),
+("Tabitha", 2, "F"),
+("Sergeant Brown", 3, "M"),
+("Melissa", 4, "F"),
+("Grinch", 5, "M"),
+("Wendy", 6, "F"),
+(NULL, 8, "M")
